96bfcbcf62ae902710b46c198ab3ec5e03962adc
Jochen <jochen.panjaer@ziango.com>
2019-12-17 12:39:06 +0100

    project structure and base assets

---
+++ Assets/Scripts/Entities/CardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    //UNITY LINKS
+
+    
+    //MEMBERS (PRIVATE)
+
+
+    //ACCESSORS - MUTATORS (PUBLIC)
+
+
+    //UNITY LIFECYCLE
+    void Awake() {
+
+    }
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+
+    //PRIVATE METHODS
+
+
+    //PUBLIC METHODS
+}
